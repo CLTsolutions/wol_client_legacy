2ea62beb2139bd870d8128d15ab0fa7bffbfec1b
Chelsey Tschida <chelsey.tschida@outlook.com>
2021-08-11 12:34:38 -0400

    finished navbar / skele workouts

--- src/home/Navbar.tsx
+++ src/home/Navbar.tsx
@@ -1,19 +1,47 @@
 import React, { Component } from 'react'
-import { Navbar, NavbarBrand } from 'reactstrap'
+import {
+  Button,
+  Collapse,
+  Nav,
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  NavItem,
+} from 'reactstrap'
 
 type acceptedProps = {
   clickLogout: () => void
 }
 
-export default class Sitebar extends Component<acceptedProps, {}> {
+interface NavbarState {
+  isOpen: boolean
+}
+
+export default class Sitebar extends Component<acceptedProps, NavbarState> {
   constructor(props: acceptedProps) {
     super(props)
+    this.state = {
+      isOpen: false,
+    }
+  }
+
+  toggle = () => {
+    let newIsOpen = !this.state.isOpen
+    this.setState({ isOpen: newIsOpen })
   }
 
   render() {
     return (
       <Navbar color='faded' light expand='md'>
         <NavbarBrand href='/'>Workout Log</NavbarBrand>
+        <NavbarToggler onClick={this.toggle} />
+        <Collapse isOpen={this.state.isOpen} navbar>
+          <Nav className='ml-auto' navbar>
+            <NavItem>
+              <Button onClick={this.props.clickLogout}>Logout</Button>
+            </NavItem>
+          </Nav>
+        </Collapse>
       </Navbar>
     )
   }
